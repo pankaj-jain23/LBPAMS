b62272c531ae3f82ded18caa41b0ad28917a5c67
sameer-nic <sameer.nic.gov@gmail.com>
2024-10-10 12:01:53 +0530

    added benchmark
    added benchmark

---
+++ LBPAMS-BEBCHMARK/ApiBenchmark.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using BenchmarkDotNet.Attributes;
+using EAMS.Controllers;
+using EAMS_ACore.IExternal;
+using EAMS_ACore.Interfaces;
+using EAMS_BLL.ExternalServices;
+using EAMS_BLL.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace LBPAMS_BEBCHMARK
+{
+    public class ApiBenchmark
+    {
+        private EAMSController _eamsController;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            // Set up DI, services, and the controller
+            var services = new ServiceCollection();
+
+            // Register your services and controllers
+            services.AddScoped<IEamsService, EamsService>();
+
+          
+            services.AddLogging();  // ILogger dependency
+            services.AddScoped<ICacheService, CacheService>();  // Your cache service registration
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            // Get the controller instance
+            var eamsService = serviceProvider.GetRequiredService<IEamsService>();
+            var mapper = serviceProvider.GetRequiredService<IMapper>();
+            var logger = serviceProvider.GetRequiredService<ILogger<EAMSController>>();
+            var cacheService = serviceProvider.GetRequiredService<ICacheService>();
+
+            _eamsController = new EAMSController(eamsService, mapper, logger, cacheService);
+        }
+
+        [Benchmark]
+        public void TestYourApiMethod()
+        {
+            // Call your controller method
+            var result = _eamsController.StateList().Result;  // or .Get() for sync
+        }
+    }
+
+
+}
---
+++ LBPAMS-BEBCHMARK/Program.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
+using BenchmarkDotNet.Toolchains.InProcess.NoEmit;
+using LBPAMS_BEBCHMARK;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var config = ManualConfig
+            .Create(DefaultConfig.Instance)
+            .AddJob(Job.Default.WithToolchain(InProcessNoEmitToolchain.Instance)); // InProcess No Emit Toolchain
+
+        var summary = BenchmarkRunner.Run<ApiBenchmark>(config);
+    }
+}
