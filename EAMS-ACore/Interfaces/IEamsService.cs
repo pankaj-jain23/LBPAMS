3d58fa6f6471c03186402a4f45d6108dcd015527
sameer-nic <sameer.nic.gov@gmail.com>
2024-10-15 18:56:10 +0530

    Revert "Reapply "Merge GetConsolidatedPanchResultDeclarationReport""
    This reverts commit 39f501261d4a4e68985e87a7d1aef97fd9697b0f.

--- EAMS-ACore/Interfaces/IEamsService.cs
+++ EAMS-ACore/Interfaces/IEamsService.cs
@@ -216,7 +216,6 @@ namespace EAMS_ACore.Interfaces
         #endregion
 
         #region Reports
-        Task<List<ConsolidatePanchResultDeclarationReportList>> GetConsolidatedPanchResultDeclarationReport(ResultDeclaration resultDeclaration);
         Task<List<ConsolidateBoothReport>> GetConsolidateBoothReports(BoothReportModel boothReportModel);
         Task<List<ConsolidateBoothReport>> GetConsolidateGPWardReports(BoothReportModel boothReportModel);
         Task<List<SoReport>> GetSOReport(BoothReportModel boothReportModel);
