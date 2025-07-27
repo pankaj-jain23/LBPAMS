9d557d47cf7013236a8542276dcfdc729b87ae97
sameer-nic <sameer.nic.gov@gmail.com>
2025-07-27 18:36:52 +0530

    Add CancelledVotes to ResultDeclaration models
    Introduced a new CancelledVotes field to the ResultDeclaration and ResultDeclarationHistory entities via a database migration. Updated the EF Core model snapshot to reflect this schema change.

---
+++ EAMS-DAL/Migrations/20250727130457_AddCancelledVotesInResultDeclaration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EAMS_DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCancelledVotesInResultDeclaration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CancelledVotes",
+                table: "ResultDeclarationHistory",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CancelledVotes",
+                table: "ResultDeclaration",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CancelledVotes",
+                table: "ResultDeclarationHistory");
+
+            migrationBuilder.DropColumn(
+                name: "CancelledVotes",
+                table: "ResultDeclaration");
+        }
+    }
+}
